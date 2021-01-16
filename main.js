eab940159396d212c4bc05241fb917ca6ca62bdc
scorpidio11 <scorpido@gmail.com>
2021-01-15 21:06:10 -0700

    Nav Menu Updated

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-const toggleBtn = document.querySelector('.mobilenav');
-const menu = document.querySelector('.mobile-menu');
+// const toggleBtn = document.querySelector('.hamburger_btn');
+// const menu = document.querySelector('.nav_menu');
 
-toggleBtn.addEventListener('click', () =>
-{
-     menu.classList.toggle('active');
+// toggleBtn.addEventListener('click', () =>
+// {
+//      menu.classList.toggle('active');
   
-});
+// });
 
+
+
+
+
+const mainMenu = document.querySelector('.nav_menu nav');
+const closeMenu = document.querySelector('.close');
+const openMenu = document.querySelector('.hamburger_btn');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
